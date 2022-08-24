3eb671fedbc8b0340e9ece1750a861bf6058f021
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-24 09:15:00 +0700

    membuat detail food

--- page/data/foods.php
+++ page/data/foods.php
@@ -103,7 +103,7 @@ if (isset($_POST['search'])) {
                                                                       Carbo : <?php echo(empty($foodsResult->food->nutrients->CHOCDF)) ? 'Data tidak tersedia' : $foodsResult->food->nutrients->CHOCDF ?> 
                                                                  </li>
                                                                  <li class="list-group-item">
-                                                                      ENERGI : <?php echo(empty($foodsResult->food->nutrients->ENERC_KCAL)) ? 'Data tidak tersedia' : $foodsResult->food->nutrients->ENERC_KCAL ?> 
+                                                                      Fiber : <?php echo(empty($foodsResult->food->nutrients->ENERC_KCAL)) ? 'Data tidak tersedia' : $foodsResult->food->nutrients->ENERC_KCAL ?> 
                                                                  </li>
                                                             </ul>
                                                        </div>
