e35d5c98ea0e82140aa4b8cb3bb236185552630c
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-24 09:43:26 +0700

    membuat detail food

--- page/data/foods.php
+++ page/data/foods.php
@@ -85,7 +85,7 @@ if (isset($_POST['search'])) {
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                        </div>
                                                        <div class="modal-body">
-                                                            <div class="text-detail">
+                                                            <div class="text-detail text-capitalize">
                                                                  nutrisi
                                                             </div>
                                                             <!-- START: List Group -->
