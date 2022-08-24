b83efe78ec9a78ea9130a00d77ccb77e5ba30495
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-24 09:03:07 +0700

    membuat komentar di halaman food

--- page/data/foods.php
+++ page/data/foods.php
@@ -79,7 +79,9 @@ if (isset($_POST['search'])) {
                                              <div class="modal-dialog">
                                                   <div class="modal-content">
                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                            <h5 class="modal-title" id="exampleModalLabel">
+                                                                 <?php echo $foodsResult->food->label ?>
+                                                            </h5>
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                        </div>
                                                        <div class="modal-body">
