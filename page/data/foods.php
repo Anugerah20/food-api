fd79a0ad2165b28737d3b508a79fa86dab89bcee
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-24 09:39:09 +0700

    membuat halaman food

--- page/data/foods.php
+++ page/data/foods.php
@@ -122,7 +122,7 @@ if (isset($_POST['search'])) {
 
                                                        </div>
                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">tutup</button>
                                                        </div>
                                                   </div>
                                              </div>
