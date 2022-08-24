423ccb77dce31ea57b6a1b5aa0688e91bec1d174
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-24 07:09:45 +0700

    update api food

--- page/data/foods.php
+++ page/data/foods.php
@@ -1,6 +1,26 @@
 <?php 
 if(isset($_POST['search'])) {
      $foods = $_POST['foods'];
-}
+
+curl_setopt_array($curl, array(
+     CURLOPT_URL => 'https://api.edamam.com/api/food-database/v2/parser?app_id=827074f0&app_key=d8e6343e9f8fa160fe7f1771f955072e&ingr=' . $food,
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_ENCODING => '',
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 0,
+     CURLOPT_FOLLOWLOCATION => true,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => 'GET',
+     CURLOPT_HTTPHEADER => array(
+       'Cookie: route=4ebff1d2ae8df64bdc65f1e53a63331e'
+     ),
+   ));
+   
+   $response = curl_exec($curl);
+   
+   curl_close($curl);
+   $data = json_decode($response);
+   
+   }
 
 ?>
