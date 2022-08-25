1f613a479d6663ee01c6fd44b442cd19c560488b
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-25 09:03:57 +0700

    membuat detail receipe

--- page/data/receipes.php
+++ page/data/receipes.php
@@ -77,7 +77,9 @@ if (isset($_POST['search'])) {
                                              <div class="modal-dialog">
                                                   <div class="modal-content">
                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                            <h5 class="modal-title" id="exampleModalLabel">
+                                                                 <?php echo $receipesResult->recipe->label ?>
+                                                            </h5>
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                        </div>
                                                        <div class="modal-body">
