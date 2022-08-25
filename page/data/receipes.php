48ce701268ee1085c1f01ab7d3a1ffa9f4435b29
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-25 08:02:15 +0700

    membuat api receipe

--- page/data/receipes.php
+++ page/data/receipes.php
@@ -2,4 +2,22 @@
 if(isset($_POST['search'])) {
      $receipes = $_POST['receipes'];
 }
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.edamam.com/api/recipes/v2?q=chicken&app_id=32ea5a75&app_key=4d43df82339606836da155153e699b1a&type=public',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
 ?>
