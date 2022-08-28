f4c5f1e0ce51547fec6615610bcb3e5e18e5cf66
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-28 14:07:43 +0700

    membuat halaman about

--- page/data/about.php
+++ page/data/about.php
@@ -3,7 +3,7 @@
           About
      </h1>
      <div class="text-center mt-5">
-          <p style="font-size: 20px;">FoodApi adalah website yang menyediakan layanan untuk mencari<br/>
+          <p style="font-size: 20px;"><span style="font-weight: 700; color: #5f54d1;">FoodApi</span> adalah website yang menyediakan layanan untuk mencari<br/>
           makanan dan resep kesukaan kamu.</p>
      </div>
 </div>
