0963b6346ae7e50c98576525e1fa158b6ba08fd7
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-28 14:29:48 +0700

    membuat halaman contact

--- page/data/contact.php
+++ page/data/contact.php
@@ -1,4 +1,4 @@
 <div class="container mt-5 mb-5">
      <h1 class="text-center text-capitalize" style="font-weight: 700; color: #5f54d1;">contact</h1>
-     <p class="text-center" style="font-size: 20px;">jika ada saran kirim pesan di bawah</p>
+     <p class="text-center mt-3" style="font-size: 20px;">jika ada saran kirim pesan di bawah</p>
 </div>
