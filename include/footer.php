a05820ecdcdd94222f57f8167a4b9446c284b4fe
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-28 14:16:29 +0700

    membuat tahun otomatis

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <footer class="mb-5 mt-5">
      <p class="text-center">
-          <small>&copy; FoodApi 2022, All Rights Reserved</small>
+          <small>&copy; FoodApi <?php echo date('Y'); ?>, All Rights Reserved</small>
      </p>
 </footer>
 
