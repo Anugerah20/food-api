381fde1d50a7de5cbc58c9ac66e2d86a6301d30f
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-28 15:00:42 +0700

    membuat title dinamis

--- include/header.php
+++ include/header.php
@@ -8,7 +8,17 @@
      <link href="assets/css/bootstrap.min.css" rel="stylesheet">
      <!-- Font -->
      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;700;900&display=swap" rel="stylesheet">
-     <title>Food Api</title>
+     <?php if (isset($_GET['home'])) { ?>
+          <title>Food Api</title>
+     <?php } else if (isset($_GET['foods'])) { ?>
+          <title>Foods</title>
+     <?php } else if(isset($_GET['receipes'])) { ?>
+          <title>Receipes</title>
+     <?php } else if (isset($_GET['about'])) { ?>
+          <title>About</title>
+     <?php } else if (isset($_GET['contact'])) { ?>
+          <title>Contact</title>
+     <?php } ?>
 </head>
 <body>
      
