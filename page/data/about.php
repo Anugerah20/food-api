c84db0aaf667aaa79f44c5f3b45c7ea7d5478dec
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-28 14:25:40 +0700

    memperbarui judul about

--- page/data/about.php
+++ page/data/about.php
@@ -1,6 +1,6 @@
 <div class="container mt-5 mb-5">
-     <h1 class="text-center" style="font-weight: 700; color: #5f54d1;">
-          About
+     <h1 class="text-center text-capitalize" style="font-weight: 700; color: #5f54d1;">
+          about
      </h1>
      <div class="text-center mt-5">
           <p style="font-size: 20px;"><span style="font-weight: 700; color: #5f54d1;">FoodApi</span> adalah website yang menyediakan layanan untuk mencari<br/>
