2dd791f01c75507a3d3e7f3684a0c585780eccde
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-28 14:00:10 +0700

    memperbaiki bagian footer

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,7 @@
 <footer class="mb-5 mt-5">
-     <p class="text-center">&copy; FoodApi 2022, All Rights Reserved</p>
+     <p class="text-center">
+          <small>&copy; FoodApi 2022, All Rights Reserved</small>
+     </p>
 </footer>
 
 <script src="js/bootstrap.bundle.min.js"></script>
