8b4af688be554b6dd9cbce71052519debff42cb1
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-29 09:05:43 +0700

    menambahkan form

--- include/header.php
+++ include/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-     <?php if (isset($_GET['foods'])) { ?>
-          <title>Foods</title>
-     <?php } else if(isset($_GET['receipes'])) { ?>
-          <title>Receipes</title>
-     <?php } else if (isset($_GET['about'])) { ?>
-          <title>About</title>
-     <?php } else if (isset($_GET['contact'])) { ?>
-          <title>Contact</title>
-     <?php } ?>
+     <title>
+          <?php 
+               if (isset($_GET['foods'])){echo "Foods";}
+               else if(isset($_GET['receipes'])){ echo "Receipes";}
+               else if (isset($_GET['about'])){ echo "About";}
+               else if (isset($_GET['contact'])){ echo "Contact";}
+               else{ echo "Home"; }
+          ?>
+     </title>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- page/data/contact.php
+++ page/data/contact.php
@@ -3,26 +3,28 @@
      <p class="text-center mt-3" style="font-size: 20px;">jika ada saran kirim pesan di bawah</p>
 
      <!-- START: Contact -->
-     <div class="row mt-5">
-          <div class="col-md-4 mx-auto">
-               <input type="text" name="nama" class="form-control" require placeholder="Your name">
-          </div>
-     </div>
-     <div class="row mt-5">
-          <div class="col-md-4 mx-auto">
-               <input type="text" name="email" class="form-control" require placeholder="Your email">
-          </div>
-     </div>
-     <div class="row mt-5">
-          <div class="col-md-4 mx-auto">
-               <textarea name="komentar" cols="10" rows="5" require placeholder="You comment" class="form-control"></textarea>
-          </div>
-     </div>
-     <div>
-          <div class="row mt-3">
+     <form action="" method="POST">
+          <div class="row mt-5">
                <div class="col-md-4 mx-auto">
-                    <button type="submit" name="send" class="btn btn-dark text-capitalize">kirim</button>
+                    <input type="text" name="nama" class="form-control" require placeholder="Your name">
                </div>
           </div>
-     </div>
+          <div class="row mt-5">
+               <div class="col-md-4 mx-auto">
+                    <input type="text" name="email" class="form-control" require placeholder="Your email">
+               </div>
+          </div>
+          <div class="row mt-5">
+               <div class="col-md-4 mx-auto">
+                    <textarea name="komentar" cols="10" rows="5" require placeholder="You comment" class="form-control"></textarea>
+               </div>
+          </div>
+          <div>
+               <div class="row mt-3">
+                    <div class="col-md-4 mx-auto">
+                         <button type="submit" name="send" class="btn btn-dark text-capitalize">kirim</button>
+                    </div>
+               </div>
+          </div>
+     </form>
 </div>
